44e5cf30c48b4f8a72415af2fde44a8295a4e20a
Khanyisile <121167505+user-k-8@users.noreply.github.com>
2023-06-30 10:15:28 +0200

    Add files via upload

---
+++ ticket-page/js/slide.js
@@ -0,0 +1,28 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+// $(document).ready(function(){
+// 	$('#mydate').datepicker({
+// 		todayHighlight: true,
+// 		 orientation: "bottom left",
+// 		 format: "mm/dd/yy",
+// 		container: ".bdc"
+// 		//container: console.log($(this).datepicker())
+// 	});
+	
+
+// });
